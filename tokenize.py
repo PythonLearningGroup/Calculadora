05f319493112fb06c05cbb5904698529f7389cfa
Juan Manuel Cruz <cruz.jm.stafe@gmail.com>
2017-12-08 01:10:06 -0300

    linted

--- tokenize.py
+++ tokenize.py
@@ -17,7 +17,7 @@ def has_operators(arg):
     return reduce(lambda c, x: c or bool(x), resultados, False)
 
 
-def unfold_list(lista, retorno=[]):
+def unfold_list(lista, retorno):
     """ Funcion que aplana una lista que puede contener listas recursivamente en su interior """
     if not lista:
         return retorno
@@ -28,15 +28,16 @@ def unfold_list(lista, retorno=[]):
     return unfold_list(lista[1:], retorno)
 
 
-def handle_operators(cadena, retorno=[]):
+def handle_operators(cadena, retorno):
     """ Si encuentra mas de un operador juntos, los separa """
-    if not len(cadena):
+    if not cadena:
         return retorno
     retorno.append(cadena[0])
     return handle_operators(cadena[1:], retorno)
 
 
-def handle_mixed(cadena, index, retorno=[]):
+def handle_mixed(cadena, index, retorno):
+    """ Si encuentra operadores y digitos sin separación, los separa """
     if not cadena:
         return retorno
     elif len(cadena) == index:
@@ -65,17 +66,10 @@ def splitter(arg):
             return handle_operators(arg, [])
         elif has_digit(arg) and has_operators(arg):
             return handle_mixed(arg, 0, [])
-        else:
-            return ''
+        return ''
 
 def tokenize(expression):
     """ Función encargada de convertir una expresión en una lista de tokens """
     tokens = expression.split(' ')
-    print(tokens)
     tokens = list(map(splitter, tokens))
-    return unfold_list(tokens)
-    # print(tokens)
-
-
-print(tokenize('3+4 *w(2+1)'))
-
+    return unfold_list(tokens, [])
